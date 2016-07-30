26729508f3061eec2aa340f4b8c8cd4ffaf34719
Gaurav Lalchandani <glalchandanig@gmail.com>
2016-07-30 12:07:44 +0530

    KeyEvents added to steer car

--- Game.java
+++ Game.java
@@ -3,11 +3,28 @@ import javax.swing.*;
 import java.awt.image.*;
 import java.net.URL;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 public class Game extends JPanel{
 
-	int crx,cry;
+	int crx,cry;				//location of cross_road
+	int car_x,car_y;			//location of car
+	
 	public Game(){
 		crx = cry = -999;
+		addKeyListener(new KeyListener() {
+			public void keyTyped(KeyEvent e) {
+			}
+			public void keyReleased(KeyEvent e) {
+				stopCar(e);
+			}
+			public void keyPressed(KeyEvent e) {
+				moveCar(e);
+			}
+		});
+		setFocusable(true);
+		car_x = car_y = 300;
 	}
 	
 	public void paint(Graphics g){
@@ -22,11 +39,10 @@ public class Game extends JPanel{
 		catch(Exception e){
 			System.out.println(e);
 		}
-		
-			obj.drawImage(getToolkit().getImage("images/car.png"),300,300,this);
+		obj.drawImage(getToolkit().getImage("images/car.png"),car_x,car_y,this);
 	}
 
-	void moveCar(int count){
+	void moveRoad(int count){
 		if(crx == -999 && cry == -999){
 			if(count%10 == 0){
 				crx = 499;
@@ -41,6 +57,25 @@ public class Game extends JPanel{
 		}
 	}
 	
+	public void moveCar(KeyEvent e){
+		if(e.getKeyCode() == KeyEvent.VK_UP){
+			car_x+=3;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_DOWN){
+			car_x-=3;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+			car_y+=3;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_LEFT){
+			car_y-=3;
+		}
+	}
+	
+	public void stopCar(KeyEvent e){
+		
+	}
+	
 	public static void main(String args[]){
 		JFrame frame = new JFrame("Car Racing Game");
 		Game game = new Game();
@@ -50,10 +85,10 @@ public class Game extends JPanel{
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		int count = 1;
     	while(true){
-    		game.moveCar(count);
+    		game.moveRoad(count);
     		game.repaint();
     		try{
-    			Thread.sleep(5);
+    			Thread.sleep(2);
     		}
     		catch(Exception e){
     			System.out.println(e);
