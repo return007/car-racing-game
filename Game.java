4518b30dc06a836c1f26ccb147d532fe12a5e5ea
Gaurav Lalchandani <glalchandanig@gmail.com>
2016-07-30 16:14:35 +0530

    Opponents added

--- Game.java
+++ Game.java
@@ -10,6 +10,13 @@ public class Game extends JPanel{
 
 	int crx,cry;				//location of cross_road
 	int car_x,car_y;			//location of car
+	int speedX,speedY;	
+	int nOpponent;
+	String imageLoc[];
+	int lx[],ly[];
+	
+	
+	boolean isUp, isDown, isRight, isLeft;
 	
 	public Game(){
 		crx = cry = -999;
@@ -25,6 +32,12 @@ public class Game extends JPanel{
 		});
 		setFocusable(true);
 		car_x = car_y = 300;
+		isUp = isDown = isLeft = isRight = false;
+		speedX = speedY = 0;
+		nOpponent = 0;
+		lx = new int[200];
+		ly = new int[200];
+		imageLoc = new String[200];
 	}
 	
 	public void paint(Graphics g){
@@ -35,11 +48,18 @@ public class Game extends JPanel{
 			obj.drawImage(getToolkit().getImage("images/st_road.png"), 0, 0 ,this);
 			if(cry >= -499 && crx >= -499)
 				obj.drawImage(getToolkit().getImage("images/cross_road.png"),crx,cry,this);
+				
+			obj.drawImage(getToolkit().getImage("images/car_self.png"),car_x,car_y,this);
+		
+			if(this.nOpponent > 0){
+				for(int i=0;i<this.nOpponent;i++){
+					obj.drawImage(getToolkit().getImage(this.imageLoc[i]),this.lx[i],this.ly[i],this);
+				}
+			}
 		}
 		catch(Exception e){
 			System.out.println(e);
 		}
-		obj.drawImage(getToolkit().getImage("images/car.png"),car_x,car_y,this);
 	}
 
 	void moveRoad(int count){
@@ -55,25 +75,73 @@ public class Game extends JPanel{
 		if(crx == -499 && cry == 0){
 			crx = cry = -999;
 		}
+		car_x += speedX;
+		car_y += speedY;
+		
+		if(car_x < 0)
+			car_x = 0;
+		
+		//also run opponents
+		for(int i=0;i<this.nOpponent;i++){
+			this.lx[i] -= 2;
+		}
+		int index[] = new int[nOpponent];
+		for(int i=0;i<nOpponent;i++){
+			if(lx[i] >= -127){
+				index[i] = 1;
+			}
+		}
+		int c = 0;
+		for(int i=0;i<nOpponent;i++){
+			if(index[i] == 1){
+				imageLoc[c] = imageLoc[i];
+				lx[c] = lx[i];
+				ly[c] = ly[i];
+				c++;
+			}
+		}
+		nOpponent = c;
 	}
 	
 	public void moveCar(KeyEvent e){
 		if(e.getKeyCode() == KeyEvent.VK_UP){
-			car_x+=3;
+			isUp = true;
+			speedX = 1;
 		}
 		if(e.getKeyCode() == KeyEvent.VK_DOWN){
-			car_x-=3;
+			isDown = true;
+			speedX = -1;
 		}
 		if(e.getKeyCode() == KeyEvent.VK_RIGHT){
-			car_y+=3;
+			isRight = true;
+			speedY = 1;
 		}
 		if(e.getKeyCode() == KeyEvent.VK_LEFT){
-			car_y-=3;
+			isLeft = true;
+			speedY = -1;
 		}
 	}
 	
 	public void stopCar(KeyEvent e){
 		
+		//System.out.println(isUp + " " + isDown + " " + isLeft + " " + isRight);
+		if(e.getKeyCode() == KeyEvent.VK_UP){
+			isUp = false;
+			speedX = 0;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_DOWN){
+			isDown = false;
+			speedX = 0;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_LEFT){
+			isLeft = false;
+			speedY = 0;
+			//System.out.println("Left key released");
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+			isRight = false;
+			speedY = 0;
+		}
 	}
 	
 	public static void main(String args[]){
@@ -83,17 +151,40 @@ public class Game extends JPanel{
 		frame.setSize(500,500);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		int count = 1;
+		int count = 1, c = 1;
     	while(true){
     		game.moveRoad(count);
-    		game.repaint();
-    		try{
-    			Thread.sleep(2);
-    		}
-    		catch(Exception e){
-    			System.out.println(e);
-    		}
-    		count++;
+    		while(c <= 1){
+				game.repaint();
+				try{
+					Thread.sleep(5);
+				}
+				catch(Exception e){
+					System.out.println(e);
+				}
+				c++;
+			}
+			c = 1;
+			count++;
+			if(game.nOpponent < 4 && count % 400 == 0){
+				game.imageLoc[game.nOpponent] = "images/car_left_"+((int)((Math.random()*100)%3)+1)+".png";
+				game.lx[game.nOpponent] = 499;
+				int p = (int)(Math.random()*100)%4;
+				if(p == 0){
+					p = 250;
+				}
+				else if(p == 1){
+					p = 300;
+				}
+				else if(p == 2){
+					p = 185;
+				}
+				else{
+					p = 130;
+				}
+				game.ly[game.nOpponent] = p;
+				game.nOpponent++;
+			}
     	}
 	}
 }
